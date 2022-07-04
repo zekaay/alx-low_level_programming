cc30ce2b8910c3cfe5eaf0df6609ffeef79b5d25
Adipah Isaac Frimpong <55854805+zekaay@users.noreply.github.com>
2022-07-05 00:18:04 +0100

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Definer
+ * Return: passer
+ */
+int main(void)
+{
+	int x = '0';
+
+	for (x = '0'; x <= '9'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
