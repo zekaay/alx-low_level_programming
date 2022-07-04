9a477348f6cdc76be8fc9fd19f7f55d73f47821d
Adipah Isaac Frimpong <55854805+zekaay@users.noreply.github.com>
2022-07-05 00:20:49 +0100

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - define
+ * Return: pass
+ */
+int main(void)
+{
+	int a = '0';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		if (a != '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
