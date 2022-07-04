918f5561f5af614c51d54d1d88cc0999866686c6
Adipah Isaac Frimpong <55854805+zekaay@users.noreply.github.com>
2022-07-05 00:20:19 +0100

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - definer
+ * Return: passer
+ */
+int main(void)
+{
+	int x = '0';
+	char y = 'a';
+
+	for (x = '0'; x <= '9'; x++)
+		putchar(x);
+	for (y = 'a'; y <= 'f'; y++)
+		putchar(y);
+	putchar('\n');
+	return (0);
+}
