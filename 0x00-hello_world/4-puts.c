0329f0c212cd73b2e104b5e2f7e064c8dd680cf8
Adipah Isaac Frimpong <55854805+zekaay@users.noreply.github.com>
2022-07-04 22:16:09 +0100

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - This prints the line
+ * Return: returns the line
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
