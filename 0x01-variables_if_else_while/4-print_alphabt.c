5dc75a38aa47136bcafe77377ab4606471f50fbf
Adipah Isaac Frimpong <55854805+zekaay@users.noreply.github.com>
2022-07-05 00:14:30 +0100

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - definer
+ * Return: passer
+ */
+int main(void)
+{
+	char lcase = 'a';
+
+	for (lcase = 'a'; lcase <= 'z'; lcase++)
+	{
+		if (lcase != 'q' && lcase != 'e')
+			putchar(lcase);
+	}
+	putchar('\n');
+	return (0);
+}
