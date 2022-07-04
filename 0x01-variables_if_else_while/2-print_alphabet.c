72e23aecb16c1f866e243e32e13dce7ac1adc90a
Adipah Isaac Frimpong <55854805+zekaay@users.noreply.github.com>
2022-07-05 00:09:05 +0100

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Main worker
+ * Return: value passer
+ */
+int main(void)
+{
+	char lcase = 'a';
+
+	for (lcase = 'a'; lcase <= 'z'; lcase++)
+	{
+		putchar(lcase);
+	}
+	putchar('\n');
+	return (0);
+}
