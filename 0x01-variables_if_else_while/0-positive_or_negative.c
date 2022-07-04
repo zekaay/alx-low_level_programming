46fc9bed31abbc06667c0874927a4881ab07e6e1
Adipah Isaac Frimpong <55854805+zekaay@users.noreply.github.com>
2022-07-05 00:01:39 +0100

    Create 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - defines what the main purpose is
+ * Return: gives a response
+ */
+int main(void)
+{
+	int n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
