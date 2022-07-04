94bf6b74649b4e64ff687185c1b683e4c35200da
Adipah Isaac Frimpong <55854805+zekaay@users.noreply.github.com>
2022-07-05 00:15:31 +0100

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - definer
+ * Return: passer
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
