2fd17a73e183881fba00a7ed680e605eaf931bc6
Adipah Isaac Frimpong <55854805+zekaay@users.noreply.github.com>
2022-07-05 00:12:07 +0100

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Definer
+ * Return: passer
+ */
+int main(void)
+{
+	char lcase = 'a';
+	char ucase = 'A';
+
+	for (lcase = 'a'; lcase <= 'z'; lcase++)
+	{
+		putchar(lcase);
+	}
+	for (ucase = 'A'; ucase <= 'Z'; ucase++)
+	{
+		putchar(ucase);
+	}
+	putchar('\n');
+	return (0);
+}
